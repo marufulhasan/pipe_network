a4108e832a69c0ba0f26b49b1fe0384faab2d3f5
Maruful Hasan <maruful.hasan@gmail.com>
2021-01-23 23:11:14 -0600

    6th commit

--- app.py
+++ app.py
@@ -7,17 +7,6 @@ app = Flask(__name__)
 app.debug = True
 app.secret_key = 'super_secret_key'
 
-test_1 = [{"data":{"id":"n0","node_type":"feed","pressure":"100"},"position":{"x":100,"y":300},"group":"nodes","removed":False,"selected":False,"selectable":True,\
-"locked":False,"grabbable":True,"classes":""},{"data":{"id":"n1","node_type":"cont","pressure":""},"position":{"x":300,"y":300},"group":"nodes","removed":False,\
-"selected":False,"selectable":True,"locked":False,"grabbable":True,"classes":""},{"data":{"id":"n2","node_type":"feed","pressure":"50"},"position":{"x":500,"y":200},\
-"group":"nodes","removed":False,"selected":False,"selectable":True,"locked":False,"grabbable":True,"classes":""},{"data":{"id":"n3","node_type":"feed","pressure":"50"},\
-"position":{"x":500,"y":400},"group":"nodes","removed":False,"selected":False,"selectable":True,"locked":False,"grabbable":True,"classes":""},\
-{"data":{"id":"e0","source":"n0","target":"n1","density":"32.2","length":"100","viscosity":"1","diameter":"4","roughness":"0.00015"},"position":{},"group":"edges",\
-"removed":False,"selected":False,"selectable":True,"locked":False,"grabbable":True,"classes":""},{"data":{"id":"e1","source":"n1","target":"n2","density":"32.2",\
-"length":"100","viscosity":"0.1","diameter":"4","roughness":"0.00015"},"position":{},"group":"edges","removed":False,"selected":False,"selectable":True,\
-"locked":False,"grabbable":True,"classes":""},{"data":{"id":"e2","source":"n1","target":"n3","density":"32.2","length":"50","viscosity":"1","diameter":"4",\
-"roughness":"0.00015"},"position":{},"group":"edges","removed":False,"selected":False,"selectable":True,"locked":False,"grabbable":True,"classes":""}]
-
 
 @app.route('/' , methods=['GET', 'POST'])
 
